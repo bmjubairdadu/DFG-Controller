e41e6684ca37327e46eda230fbca2c723a8036cd
bmjubairdadu <bmjubairdadu@users.noreply.github.com>
2026-08-11 05:56:42 -0700

    Bump version to 1.4.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -17,8 +17,8 @@ android {
         applicationId = "com.daisyforgaming"
         minSdk = 26
         targetSdk = 35
-        versionCode = 5
-        versionName = "1.3.0"
+        versionCode = 6
+        versionName = "1.4.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
